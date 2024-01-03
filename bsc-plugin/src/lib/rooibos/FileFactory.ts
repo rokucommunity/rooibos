8404ca7445c60e387a9416067c0937953b477f2b
Christopher Dwyer-Perkins <chris@inverted-solutions.com>
2024-01-03 12:49:02 -0400

    fixed horiz alignment

--- bsc-plugin/src/lib/rooibos/FileFactory.ts
+++ bsc-plugin/src/lib/rooibos/FileFactory.ts
@@ -89,7 +89,7 @@ export class FileFactory {
 
                 <children>
                     <Rectangle id="statusBackground" color="#444444" width="1920" height="1080" />
-                    <LayoutGroup translation="[960, 540]" vertAlignment="center" >
+                    <LayoutGroup translation="[960, 540]" vertAlignment="center"  horizAlignment="center">
                         <Label id="statusLabel" text='Rooibos is running tests' width="1800" />
                         <Label id="failedLabel" text="" translation="[0, 100]" width="1800" wrap="true" maxLines="15"/>
                     </LayoutGroup>
