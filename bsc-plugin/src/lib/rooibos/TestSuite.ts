d81fc64f9bdee8f4a28e7e4a6e76c293b0a52c42
Christopher Dwyer-Perkins <chris@inverted-solutions.com>
2024-07-02 13:52:41 -0300

    Fixed a bug where scope validations where incomplete for node tests (#280)
    * Fixed a bug where scope validations where incomplete for node tests
    
    * Update bsc-plugin/src/plugin.spec.ts
    
    * Fix lint issues.
    
    ---------
    
    Co-authored-by: Bronley Plumb <bronley@gmail.com>

--- bsc-plugin/src/lib/rooibos/TestSuite.ts
+++ bsc-plugin/src/lib/rooibos/TestSuite.ts
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import type { AstEditor, BrsFile, ClassStatement } from 'brighterscript';
 
 import { diagnosticNodeTestIllegalNode, diagnosticNodeTestRequiresNode } from '../utils/Diagnostics';
@@ -58,6 +59,8 @@ export class TestBlock {
     public tearDownFunctionName: string;
     public beforeEachFunctionName: string;
     public afterEachFunctionName: string;
+    public xmlPkgPath: string;
+    public bsPkgPath: string;
 
 }
 
@@ -71,7 +74,9 @@ export class TestSuite extends TestBlock {
             this.annotation.name = classStatement.name.text;
         }
         this.generatedNodeName = (this.name || 'ERROR').replace(/[^a-zA-Z0-9]/g, '_');
-
+        let pathBase = path.join('components', 'rooibos', 'generated');
+        this.xmlPkgPath = path.join(pathBase, this.generatedNodeName + '.xml');
+        this.bsPkgPath = path.join(pathBase, this.generatedNodeName + '.bs');
     }
 
     //state
