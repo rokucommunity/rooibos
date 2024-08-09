0351b77ff413a4880f3df8ef3ebc1e91d7e914e9
Mark Pearce <mark.pearce@redspace.com>
2024-08-09 13:55:23 -0300

    Fixed lint errors in tests

--- bsc-plugin/src/lib/rooibos/RawCodeStatement.ts
+++ bsc-plugin/src/lib/rooibos/RawCodeStatement.ts
@@ -1,10 +1,11 @@
 import type {
-    AstNodeKind,
     BscFile,
     WalkOptions,
     WalkVisitor
 } from 'brighterscript';
 import {
+    AstNodeKind,
+    Expression,
     Range,
     Statement,
     util
@@ -15,6 +16,40 @@ import { SourceNode } from 'source-map';
 import type { BrsTranspileState } from 'brighterscript/dist/parser/BrsTranspileState';
 
 export class RawCodeStatement extends Statement {
+
+    constructor(
+        public source: string,
+        public sourceFile?: BscFile,
+        public range: Range = Range.create(1, 1, 1, 99999)
+    ) {
+        super();
+    }
+
+    readonly kind = AstNodeKind.Body;
+
+    get location() {
+        return util.createLocationFromFileRange(this.sourceFile, this.range);
+    }
+
+    public transpile(state: BrsTranspileState) {
+        //indent every line with the current transpile indent level (except the first line, because that's pre-indented by bsc)
+        let source = this.source.replace(/\r?\n/g, (match, newline) => {
+            return state.newline + state.indent();
+        });
+
+        return [new SourceNode(
+            this.range.start.line + 1,
+            this.range.start.character,
+            this.sourceFile ? this.sourceFile.srcPath : state.srcPath,
+            source
+        )];
+    }
+    public walk(visitor: WalkVisitor, options: WalkOptions) {
+        //nothing to walk
+    }
+}
+
+export class RawCodeExpression extends Expression {
     constructor(
         public source: string,
         public sourceFile?: BscFile,
@@ -24,7 +59,7 @@ export class RawCodeStatement extends Statement {
     }
 
 
-    readonly kind = 'RawCodeExpression' as AstNodeKind;
+    readonly kind = AstNodeKind.Block;
 
     get location() {
         return util.createLocationFromFileRange(this.sourceFile, this.range);
