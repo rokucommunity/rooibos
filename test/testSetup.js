2be3512f1997e1341022032a5464ab81f538e872
Luis Soares <luis.soares@sky.uk>
2026-04-28 16:53:34 +0100

    Remove Node 16 fetch shim after roku-deploy 3.17.1 release

--- test/testSetup.js
+++
@@ -1,9 +0,0 @@
-if (typeof globalThis.fetch !== 'function') {
-    Object.defineProperty(globalThis, 'fetch', {
-        configurable: true,
-        writable: true,
-        value: async function fetch() {
-            throw new Error('Unexpected fetch call during tests on Node 16');
-        }
-    });
-}
